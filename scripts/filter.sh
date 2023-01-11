20f292caef1d96ca06cf9f217b90fa6987ec2eaa
Ambre Austen Suhamy <ambre@tarides.com>
2023-01-11 15:36:45 +0100

    Linting shell scripts

--- scripts/filter.sh
+++ scripts/filter.sh
@@ -3,12 +3,12 @@
 CONFIG_SWITCH_NAME=$1
 RUN_CONFIG_JSON=$2
 
-echo $CONFIG_SWITCH_NAME | grep -qE '.*(4\.14|5\.0\.1|5\.1\.0).*'
-if [ $? -eq 0 ]; then
+echo "$CONFIG_SWITCH_NAME" | grep -qE '.*(4\.14|5\.0\.1|5\.1\.0).*'
+if [ $? = 0 ]; then
     # Recognized compiler variant (4.14, 5.0.1, 5.1.0)
     echo "Filtering some benchmarks for OCaml $CONFIG_SWITCH_NAME";
     jq '{wrappers : .wrappers, benchmarks: [.benchmarks | .[] | select( .name as $name | ["irmin_replay", "cpdf", "frama-c", "js_of_ocaml", "graph500_kernel1", "graph500_kernel1_multicore"] | index($name) | not )]}' $RUN_CONFIG_JSON >$RUN_CONFIG_JSON.tmp;
-    mv $RUN_CONFIG_JSON.tmp $RUN_CONFIG_JSON;
+    mv "$RUN_CONFIG_JSON".tmp "$RUN_CONFIG_JSON";
     echo "(data_only_dirs irmin cpdf frama-c)" >benchmarks/dune
 else
     echo "Not filtering benchmarks for OCaml $CONFIG_SWITCH_NAME"
--- scripts/loadavg.sh
+++ scripts/loadavg.sh
@@ -10,15 +10,14 @@ loadavg () {
 if [ "$OPT_WAIT" = 1 ]; then 
     wall "It's bench startup time, please clear the way!";
     sleep 60
-    while [ $(loadavg) -gt 60 ]; do 
-        if [ $(expr $(date +%s) - $START_TIME) -gt $(expr 3600 \* 12) ]; then 
-            echo "Could not start for the past 12 hours; aborting run" >&2; 
-            exit 10; 
-        else 
-            echo "System load detected, waiting to run bench (retrying in 5 minutes)"; 
-            echo "Loadavg: $(loadavg)"; 
-            wall "It's bench startup time, but the load is too high. Please clear the way!"; 
-            sleep 300; 
-        fi; 
+    while [ "$(loadavg)" -gt 60 ]; do 
+        if [ "$(($(date +%s) - START_TIME > 3600 * 12))" ]; then 
+            echo "Could not start for the past 12 hours; aborting run" >&2;
+            exit 10;
+        fi;
+        echo "System load detected, waiting to run bench (retrying in 5 minutes)";
+        echo "Loadavg: $(loadavg)";
+        wall "It's bench startup time, but the load is too high. Please clear the way!";
+        sleep 300;
     done; 
 fi
