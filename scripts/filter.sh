fef0dbd39e4a2bf55634067f8185887466d7b4d1
Ambre Austen Suhamy <ambre@tarides.com>
2023-01-16 15:31:51 +0100

    Fix yet another shell quoting issue

--- scripts/filter.sh
+++ scripts/filter.sh
@@ -7,7 +7,7 @@ echo "$CONFIG_SWITCH_NAME" | grep -qE '.*(4\.14|5\.0\.1|5\.1\.0).*'
 if [ $? = 0 ]; then
     # Recognized compiler variant (4.14, 5.0.1, 5.1.0)
     echo "Filtering some benchmarks for OCaml $CONFIG_SWITCH_NAME";
-    jq '{wrappers : .wrappers, benchmarks: [.benchmarks | .[] | select( .name as $name | ["irmin_replay", "cpdf", "frama-c", "js_of_ocaml", "graph500_kernel1", "graph500_kernel1_multicore"] | index($name) | not )]}' $RUN_CONFIG_JSON >$RUN_CONFIG_JSON.tmp;
+    jq '{wrappers : .wrappers, benchmarks: [.benchmarks | .[] | select( .name as $name | ["irmin_replay", "cpdf", "frama-c", "js_of_ocaml", "graph500_kernel1", "graph500_kernel1_multicore"] | index($name) | not )]}' "$RUN_CONFIG_JSON" >"$RUN_CONFIG_JSON".tmp;
     mv "$RUN_CONFIG_JSON".tmp "$RUN_CONFIG_JSON";
     echo "(data_only_dirs irmin cpdf frama-c)" >benchmarks/dune
 else
