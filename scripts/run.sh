3ecc6b993bf9c5ed69f867fcbc275901d61e1fa5
Ambre Austen Suhamy <ambre@tarides.com>
2023-01-18 14:54:52 +0100

    Linting run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -18,22 +18,22 @@ echo "Executing benchmarks with:";
 echo "  RUN_CONFIG_JSON=$run_config_json";
 echo "  RUN_BENCH_TARGET=$run_bench_target  (WRAPPER=$wrapper)";
 echo "  PRE_BENCH_EXEC=$pre_bench_exec";
-"$pre_bench_exec" "$ENVIRONMENT" opam exec --switch "$config_switch_name" -- dune build -j 1 --profile=release --workspace=ocaml-versions/.workspace."$config_switch_name" @"$run_bench_target"; ex=$?;
+# The big exec is here
+$pre_bench_exec opam exec --switch "$config_switch_name" -- dune build -j 1 --profile=release --workspace=ocaml-versions/.workspace."$config_switch_name" @"$run_bench_target"; ex=$?;
+
+metadata () {
+    arch=$(uname -m);
+    hostname=$(hostname);
+    kernel=$(uname -s);
+    version=$(uname -r);
+
+    jo arch="$arch" hostname="$hostname" kernel="$kernel" version="$version"
+}
+
 mkdir -p _results/;
 cp "$run_config_json" _results/;
 for i in $(seq 1 "$iter"); do
-    declare -A META=( ["arch"]="uname -m" ["hostname"]="hostname" ["kernel"]="uname -s" ["version"]="uname -r" );
-    s="";
-    for key in "${!META[@]}"; do
-        result=$(${META[${key}]});
-        if [ -z "$s" ]; then
-            s="$key=$result";
-        else
-            s="$s $key=$result";
-        fi;
-    done;
-    header_entry=$(jo -p "$s" | jq -c);
-    echo "$header_entry" > _results/"${sandmark_custom_name}"_"$i"."$wrapper".summary.bench;
+    metadata > _results/"$sandmark_custom_name"_"$i"."$wrapper".summary.bench;
     find _build/"$config_switch_name"_"$i" -name '*.'"$wrapper"'.bench' -print0 | xargs cat >> _results/"$sandmark_custom_name"_"$i"."$wrapper".summary.bench;
 done;
 exit "$ex"
